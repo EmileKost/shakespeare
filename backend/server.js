17a7ed4b891976f67f13d80c6431e1c070d1d172
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-02-19 12:12:56 +0100

    feat: setting up controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const port = 5000;
 const app = express();
 
+app.get("/", (req, res) => {
+	res.send("I am a test server");
+});
+
 app.listen(app, () => {
 	console.log(`Server is running on port: ${port}`);
 });
